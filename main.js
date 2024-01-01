67d5f49322dfec01c440134903e3fcdfe3965fda
oussama <oussama.hel@gmail.com>
2024-01-01 10:35:56 +0100

    code refactoring

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ const PROJECT_ID = process.env.PROJECT_ID;
 const DB_ID = process.env.DB_ID;
 const COLLECTION_ID_PROJECTS = process.env.COLLECTION_ID_PROJECTS;
 
+async function getPosts(parser, subreddit) {
+    const feed = await parser.parseURL(subreddit);
+    return feed.items
+}
+
+async function savePosts(db, posts) {
+    for (let post of posts) {
+        const result = await db.listDocuments(DB_ID, COLLECTION_ID_PROJECTS, [
+            Query.equal('url', post.link)
+        ]);
+        if (result.total === 0) {
+            log(post.link);
+            await db.createDocument(DB_ID, COLLECTION_ID_PROJECTS, ID.unique(), {
+                author: post.author,
+                content: post.contentSnippet,
+                contentHTML: post.content,
+                title: post.title,
+                pubDate: new Date(post.pubDate),
+                url: post.link,
+                subreddit: 'htmx',
+            })
+        }
+    }
+}
 
 export default async ({ req, res, log, err }) => {
 
@@ -28,24 +52,9 @@ export default async ({ req, res, log, err }) => {
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
                 'accept': 'text/html,application/xhtml+xml'
             });
-        let feed = await parser.parseURL('https://www.reddit.com/r/htmx.rss');
 
-        for (let item of feed.items) {
-            const result = await db.listDocuments(DB_ID, COLLECTION_ID_PROJECTS, [
-                Query.equal('url', item.link)
-            ]);
-            if (result.total === 0) {
-                log(item.link);
-                await db.createDocument(DB_ID, COLLECTION_ID_PROJECTS, ID.unique(), {
-                    author: item.author,
-                    content: item.contentSnippet,
-                    contentHTML: item.content,
-                    title: item.title,
-                    pubDate: new Date(item.pubDate),
-                    url: item.link,
-                })
-            }
-        }
+        const posts = getPosts(parser, 'https://www.reddit.com/r/htmx.rss')
+        await savePosts(db, posts);
     }
     return res.empty()
 }
